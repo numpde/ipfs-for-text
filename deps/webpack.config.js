b6bf50b8e9e92164189e292b6646a3f808785eae
RA <numpde@null.net>
2023-06-13 09:56:24 +0400

    `window.getHash`

--- deps/webpack.config.js
+++ deps/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './index.js',
     output: {
-        filename: 'hash.js',
+        filename: 'gethash.js',
         path: path.resolve(__dirname, 'dist'),
     },
 };
